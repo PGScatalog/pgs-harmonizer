3c2c4ea697c40334e344ef5be309d991ecf00ed8
smlmbrt <sam.a.lambert@gmail.com>
2020-04-02 17:22:43 +0100

    Map rsID -> genomic location (in build of choice) using ENSEMBL API (POST)

--- Harmonize.py
+++ Harmonize.py
@@ -1,8 +1,42 @@
-from pgs_harmonizer.api_tools import ensembl_post
-import pandas as pd
+from pgs_harmonizer.ensembl_tools import ensembl_post
+from pgs_harmonizer.scorefile_IO import read_ScoreFile
+import
+
+#Globals
+chromosomes = [str(x) for x in range(1,23)] + ['X', 'Y']
+
+
+build_target = 'GRCh38'
 
 loc_scorefile = '../pgs_ScoringFiles/PGS000001.txt.gz'
-df_scoring = pd.read_table(loc_scorefile, float_precision='round_trip', comment = '#')
 
-#Get mappings for 38
-mapping_ensembl = ensembl_post(list(df_scoring['rsID']))
+header, df_scoring  = read_ScoreFile(loc_scorefile)
+
+# Sorting out the genome build
+mappable = False
+if header['genome_build'] == None:
+    if 'rsID' in df_scoring.columns:
+        if all([x.startswith('rs') for x in df_scoring['rsID']]):
+            mappable = True #By rsID
+    # ToDo guess the genome build
+else:
+    mappable = True
+
+# Getting the Chromosome positions
+if header['genome_build'] != build_target:
+    df_scoring['hm_chr'] = ''
+    df_scoring['hm_pos'] = int()
+
+    # First try and map everything by rsID (if available)
+    if 'rsID' in df_scoring.columns:
+        mapping_ensembl = ensembl_post(list(df_scoring['rsID']), build_target) #retireve the SNP info from ENSEMBL
+        for i, v in df_scoring.iterrows():
+            v_map = mapping_ensembl.get(v['rsID'])
+            if v_map is not None:
+                df_scoring.loc[i,['hm_chr', 'hm_pos']] = v_map.select_canonical_data(chromosomes)
+
+    # Check if everything has a chr/pos
+
+
+
+
--- pgs_harmonizer/ensembl_tools.py
+++ pgs_harmonizer/ensembl_tools.py
@@ -1,7 +1,35 @@
 import requests
+import pandas
 from requests.adapters import HTTPAdapter
 from requests.exceptions import ConnectionError
 
+
+class VariantResult:
+    def __init__(self, id, json_result):
+        self.id = id
+        self.json_result = json_result
+
+    def select_canonical_data(self, chromosomes):
+        '''To identify the best mapping (adapted from GWAS Catalog)'''
+
+        mapped_data = self.json_result['mappings']
+
+        chrom = []
+        bp = []
+        for mapping in mapped_data:
+            if mapping['seq_region_name'] in chromosomes:
+                # print(mapping['seq_region_name'], mapping['start'])
+                bp.append(mapping['start'])
+                chrom.append(mapping['seq_region_name'])
+        if len(bp) == 1:
+            return chrom[0], bp[0]
+        elif len(bp) > 1 and all_same(bp):
+            return chrom[0], bp[0]  # "AMBIGUOUS"
+        else:
+            return None, None  # to catch those where they only map to a patch
+
+def all_same(items):
+    return all(x == items[0] for x in items)
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
@@ -33,7 +61,9 @@ def ensembl_post(rsid_list, build = 'GRCh38'):
         payload = {'ids': c_ids }
         try:
             r = session.post(url + '/variation/homo_sapiens', headers=headers, json=payload)
-            results.update(r.json())
+            for i,j in r.json().items():
+                results[i] = VariantResult(i, j)
         except ConnectionError as ce:
             print(ce)
     return(results)
+
