588af4a1d304db9f0ddd6a64408628c0f21f9adc
smlmbrt <sam.a.lambert@gmail.com>
2022-05-06 10:58:59 +0100

    Streamlined/validating VCF downloading script

--- DownloadMappings.py
+++ DownloadMappings.py
@@ -1,19 +1,42 @@
+import argparse
 import os
 import ftplib
 
-# Check/create structure to hold VCF files
-if os.path.isdir('map/') is False:
-    os.mkdir('map/')
-if os.path.isdir('map/vcf_ref/') is False:
-    os.mkdir('map/vcf_ref/')
 
-vcf_builds = ['GRCh37', 'GRCh38']
-vcf_chrs = list(range(1,23)) + ['X', 'Y', 'MT']
+vcf_chrs = [str(x) for x in range(1,23)] + ['X', 'Y', 'MT']
+
+parser = argparse.ArgumentParser(
+    description='Download VCFs from ENSEMBL.')
+parser.add_argument(dest="genome_build",
+                    help="Target genome build choices: GRCh37, GRCh38, or both (GRCh3*)", metavar="GRCh3#",
+                    choices=['GRCh37', 'GRCh38', 'GRCh3*'])
+parser.add_argument(dest="loc_outputs",
+                    help="Directory where the VCF files will be saved (default: PGS_HmPOS/)",
+                    metavar="OUTDIR")
+parser.add_argument('-chrom', dest='single_chrom',
+                    help='Option to download a single chrom, otherwise all chromosomes will be downloaded',
+                    metavar="CHR", choices=vcf_chrs, required=False)
+args = parser.parse_args()
+
+# Check/create structure to hold VCF files
+if os.path.isdir(args.loc_outputs) is False:
+    os.mkdir(args.loc_outputs)
+
+if args.genome_build == 'GRCh3*':
+    vcf_builds = ['GRCh37', 'GRCh38']
+else:
+    vcf_builds = [args.genome_build]
 
 for build in vcf_builds:
     if os.path.isdir('map/vcf_ref/{}'.format(build)) is False:
         os.mkdir('map/vcf_ref/{}'.format(build))
         os.mkdir('map/vcf_ref/{}/cohort_ref'.format(build))
+
+# Check which chromosomes to download
+if args.single_chrom is not None:
+    dl_chrs = [args.single_chrom]
+else:
+    dl_chrs = vcf_chrs
 
 # Download VCF files from ENSEMBL FTP
 ensembl_ftp = ftplib.FTP("ftp.ensembl.org")
@@ -24,7 +47,7 @@ for build in vcf_builds:
     if build == 'GRCh37':
         vcf_loc = 'pub/grch37/current/variation/vcf/homo_sapiens/'
 
-    for chr in vcf_chrs:
+    for chr in dl_chrs:
         print('Downloading (vcf):', build, 'chr{}'.format(chr))
         try:
             # Download vcf
