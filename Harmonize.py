f8730cd9cea76bead2c6c26e95b4c1dd18c02614
Laurent Gil <lg10@sanger.ac.uk>
2023-10-18 10:15:11 +0100

    Check if the file need a change write access before attempting to change it

--- Harmonize.py
+++ Harmonize.py
@@ -634,12 +634,17 @@ def run_HmVCF(args):
 
 
 def change_file_write_acces(filename):
-    # Change chmod to allow group write access
+    # Try to change chmod to allow group write access
     if os.path.isfile(filename):
-        try:
-            os.chmod(filename, stat.S_IRUSR|stat.S_IWUSR|stat.S_IRGRP|stat.S_IWGRP|stat.S_IROTH)
-        except:
-            print(f">>>>> ERROR! Can't change the read/write access of the file '{filename}'!")
+        file_stat = os.stat(filename)
+        file_mode = file_stat.st_mode
+        # Check if the file already has group write permission
+        if not stat.S_IWGRP & file_mode:
+            try:
+                # Change chmod to "-rw-rw-r--"
+                os.chmod(filename, stat.S_IRUSR|stat.S_IWUSR|stat.S_IRGRP|stat.S_IWGRP|stat.S_IROTH)
+            except:
+                print(f">>>>> ERROR! Can't change the read/write access of the file '{filename}'!")
 
 
 if __name__ == "__main__":
